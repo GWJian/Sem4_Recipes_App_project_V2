a4009f9a303edbf971cb71de81f248707af90335
Goh Wai Jian <gwaijian@gmail.com>
2023-11-16 21:06:15 +0800

    complete fire base -FireStore -Realtime -Authentication -Storage

--- app/src/main/java/com/gwj/recipesappV2/MainActivity.kt
+++ app/src/main/java/com/gwj/recipesappV2/MainActivity.kt
@@ -2,6 +2,10 @@ package com.gwj.recipesappV2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.database.ktx.database
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -9,5 +13,31 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        val db = Firebase.firestore
+//
+//        // Create a new user with a first and last name
+//        val user = hashMapOf(
+//            "first" to "Ada",
+//            "last" to "Lovelace",
+//            "born" to 1815
+//        )
+//
+//// Add a new document with a generated ID
+//        db.collection("users")
+//            .add(user)
+//            .addOnSuccessListener { documentReference ->
+//                Log.d("debugging", "DocumentSnapshot added with ID: ${documentReference.id}")
+//            }
+//            .addOnFailureListener { e ->
+//                Log.w("debugging", "Error adding document", e)
+//            }
+//
+
+//        // Write a message to the database
+//        val database = Firebase.database
+//        val myRef = database.getReference("message")
+//
+//        myRef.setValue("Hello, World!")
     }
 }
