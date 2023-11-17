7a54f98ba0fc87aa45105684cbc9a99cfc1ae45a
Goh Wai Jian <gwaijian@gmail.com>
2023-11-17 13:22:27 +0800

    profile page done -logout -upload img -get img name email

--- app/src/main/java/com/gwj/recipesappV2/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/gwj/recipesappV2/ui/profile/ProfileViewModel.kt
@@ -1,8 +1,76 @@
 package com.gwj.recipesappV2.ui.profile
 
-import androidx.lifecycle.ViewModel
+import android.net.Uri
+import androidx.lifecycle.viewModelScope
+import com.gwj.recipesappV2.core.service.AuthService
+import com.gwj.recipesappV2.core.service.StorageService
+import com.gwj.recipesappV2.data.model.User
+import com.gwj.recipesappV2.data.repo.UserRepo
+import com.gwj.recipesappV2.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class ProfileViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@HiltViewModel
+class ProfileViewModel @Inject constructor(
+    private val authService: AuthService,
+    private val storageService: StorageService,
+    private val userRepo: UserRepo
+) : BaseViewModel() {
+    private val _user = MutableStateFlow(User(name = "Unknown", email = "Unknown"))
+    val user: StateFlow<User> = _user
+
+    private val _profileUri = MutableStateFlow<Uri?>(null)
+    val profileUri: StateFlow<Uri?> = _profileUri
+
+    private val _finish = MutableSharedFlow<Unit>()
+    val finish: SharedFlow<Unit> = _finish
+
+    init {
+        getCurrentUser()
+        getProfilePicUri()
+    }
+
+    fun logout() {
+        authService.logout()
+        viewModelScope.launch {
+            _finish.emit(Unit)
+        }
+    }
+
+    fun updateProfilePic(uri: Uri) {
+        user.value.id?.let {
+            viewModelScope.launch(Dispatchers.IO) {
+                val name = "${it}.jpg"
+                storageService.addImage(name, uri)
+                getProfilePicUri()
+            }
+        }
+    }
+
+    fun getProfilePicUri() {
+        viewModelScope.launch(Dispatchers.IO) {
+            authService.getCurrentUser()?.uid.let {
+                _profileUri.value = storageService.getImage("${it}.jpg")
+            }
+        }
+    }
+
+    fun getCurrentUser() {
+        val firebaseUser = authService.getCurrentUser()
+        firebaseUser?.let {
+            viewModelScope.launch(Dispatchers.IO) {
+                safeApiCall { userRepo.getUser(it.uid) }?.let { user ->
+                    _user.value = user
+                }
+            }
+        }
+    }
+
+
 }
