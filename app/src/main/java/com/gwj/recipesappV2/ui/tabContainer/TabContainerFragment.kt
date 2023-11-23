b65519594cc921e5259c8c47c6f86600c947ea82
gwaijian <gwaijian@gmail.com>
2023-11-23 16:10:15 +0800

    popback alert

--- app/src/main/java/com/gwj/recipesappV2/ui/tabContainer/TabContainerFragment.kt
+++ app/src/main/java/com/gwj/recipesappV2/ui/tabContainer/TabContainerFragment.kt
@@ -1,9 +1,11 @@
 package com.gwj.recipesappV2.ui.tabContainer
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import com.google.android.material.tabs.TabLayoutMediator
 import com.gwj.recipesappV2.R
@@ -15,6 +17,26 @@ import com.gwj.recipesappV2.ui.profile.ProfileFragment
 class TabContainerFragment : Fragment() {
 
     private lateinit var binding: FragmentTabContainerBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val callback = object : OnBackPressedCallback(true /* enabled by default */) {
+            override fun handleOnBackPressed() {
+                // Show a confirmation dialog
+                AlertDialog.Builder(requireContext())
+                    .setTitle("Exit App")
+                    .setMessage("Are you sure you want to exit?")
+                    .setPositiveButton("Yes") { _, _ ->
+                        // Exit app
+                        activity?.finish()
+                    }
+                    .setNegativeButton("No", null)
+                    .show()
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(this, callback)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
